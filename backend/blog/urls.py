3a2a5731c24a93e6a6fda5a5ca8f6d924de408f4
carlos reyes <carlosarp2000@gmail.com>
2022-04-12 23:42:50 -0500

    manejo de slug, queda pendiente mejorar y usuarios

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,11 +1,13 @@
 import imp
 from django.urls import path
 from .views import (
-    BlogHomePageView, 
+    BlogHomePageView,
+    DetailView
     )
 
 app_name = 'blog'
 
 urlpatterns = [
     path("", BlogHomePageView.as_view(), name="home"),
+    path("<slug:slug>/", DetailView.as_view(), name="post_detail"),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+
 from .models import Post
 
 class BlogHomePageView(TemplateView):
@@ -8,4 +9,15 @@ class BlogHomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.postobjects.all()
+        return context
+
+
+class DetailView(DetailView):
+    model = Post
+    template_name = "blog/post_detail.html"
+    context_object_name = "post"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        post = Post.objects.filter(slug = self.kwargs.get('slug'))
         return context
